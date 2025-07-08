e66fb999c53782255b82b83a1669a845fe68ac21
Atindra Ghosh <atindraghosh2017@gmail.com>
2025-07-08 21:24:32 +0530

    updated app and home

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import EditorPage from './pages/EditorPage';
 import Home from './pages/Home'
+import { Toaster } from 'react-hot-toast';
 function App() {
   return (
     <>
+      <div>
+        <Toaster
+          position='center'
+          toastOptions={{
+            success:{
+              theme:{
+                primary:"#00396e"
+              }
+            }
+          }}
+        ></Toaster>
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>}></Route> 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import toast from 'react-hot-toast';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  
+  const [roomId, setRoomId] = useState('');
+  const [username, setUsername] = useState('');
+  const createNewRoom = (e) => {
+    e.preventDefault();
+    const id = uuidV4();
+    setRoomId(id);
+    toast.success("Created a new room");
+  };
 
-export default Home
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img
+          className="homePageLogo"
+          src="/code-sync.png"
+          alt="code-sync-logo"
+        />
+        <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+        <div className="inputGroup">
+
+          <input type="text" className="inputBox" placeholder="ROOM ID"
+          onChange={(e)=>setRoomId(e.target.value)} 
+          value={roomId}/>
+
+          <input type="text" className="inputBox" placeholder="USERNAME"
+          onChange={(e)=>setUsername(e.target.value)} 
+          value={username} />
+          <button className="btn joinBtn">Join</button>
+          <span className="createInfo">
+            If you don't have an invite then create &nbsp;
+            <a onClick={createNewRoom} href=" " className="createNewBtn">
+              new room
+            </a>
+          </span>
+        </div>
+      </div>
+      <footer>
+        <h4>
+          Built by &nbsp;
+          <a href="https://github.com/WEBX2024">Atindra Ghosh</a>
+        </h4>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
