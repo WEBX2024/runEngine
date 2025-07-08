70588085c9b88fe2b85040fdeedaa72a28112bd4
Atindra Ghosh <atindraghosh2017@gmail.com>
2025-07-08 12:24:09 +0530

    project start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import EditorPage from './pages/EditorPage';
+import Home from './pages/Home'
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route> 
+          <Route path='/editor/:roomid' element={<EditorPage/>}></Route> 
+        </Routes>
+      </BrowserRouter>
+    
+    </>
   );
 }
 
